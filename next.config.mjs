7aa2b45c3bfba695eb46bf5cebcb4b7ee20299a5
adahcodxd <amritanshujha96@gmail.com>
2024-07-04 11:47:02 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const nextConfig = {
+  reactStrictMode: true,
+  env: {
+    TOKEN_SECRET: process.env.TOKEN_SECRET,
+    DOMAIN: process.env.DOMAIN,
+  },
+};
 
 export default nextConfig;
